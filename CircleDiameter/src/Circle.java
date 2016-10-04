e8289e30a263f1b323b8706d3ff46fa12fea5c3b
Chase Howard <chasehoward85@gmail.com>
2016-10-04 12:31:11 -0600

    Project

---
+++ CircleDiameter/src/Circle.java
@@ -0,0 +1,13 @@
+
+public class Circle {
+	private double radius;
+	
+	public Circle(double r){
+		radius = r;
+	}
+	
+	public double diameter(){
+		double diam = radius * 2;
+		return diam;
+	}
+}
---
+++ CircleDiameter/src/Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		Circle circ1 = new Circle(20);
+		System.out.println(circ1.diameter());
+
+	}
+
+}
